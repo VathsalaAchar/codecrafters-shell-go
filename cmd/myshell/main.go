c3b7ee8490f6d7bde4e5c7aad36596538b6e9e6e
Vathsala <vathsala.sachar@gmail.com>
2025-01-22 16:33:53 +0000

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,15 +7,16 @@ import (
 	"strings"
 )
 
-
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		// Wait for user input
+		cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	// remove the newline from the end of cmd
-	cmd = strings.TrimSuffix(cmd, "\n")
-	fmt.Printf("%s: command not found", cmd)
+		// remove the newline from the end of cmd
+		cmd = strings.TrimSuffix(cmd, "\n")
+		fmt.Printf("%s: command not found\n", cmd)
+	}
 }
