2c305d1dc3a4304567ca61256b6766ec0bb4f7a6
Vathsala <vathsala.sachar@gmail.com>
2025-01-24 15:08:42 +0000

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -17,12 +17,22 @@ func main() {
 		// remove the newline from the end of cmd
 		cmd := strings.TrimSuffix(input, "\n")
 
-		switch cmd {
-		case "exit 0":
-			os.Exit(0)
-		default:
-			msg := fmt.Sprintf("%s: command not found\n", cmd)
-			fmt.Print(msg)
-		}
+		run_command(cmd)
+	}
+}
+
+func run_command(cmd string) {
+	// built-in commands
+	// builtInCommands :=
+
+	switch {
+	case cmd == "exit 0":
+		os.Exit(0)
+	case strings.HasPrefix(cmd, "echo"):
+		echo_msg := strings.TrimSpace(strings.TrimLeft(cmd, "echo"))
+		fmt.Println(echo_msg)
+	default:
+		msg := fmt.Sprintf("%s: command not found\n", cmd)
+		fmt.Print(msg)
 	}
 }
