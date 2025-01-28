d0ba967b4c3529ddc64578c4877872b3f55fd9a8
Vathsala <vathsala.sachar@gmail.com>
2025-01-28 11:07:05 +0000

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -22,12 +23,22 @@ func main() {
 }
 
 func run_command(cmd string) {
+	builtin_cmds := []string{"echo", "exit", "type"}
 	switch {
 	case cmd == "exit 0":
 		os.Exit(0)
 	case strings.HasPrefix(cmd, "echo"):
 		echo_msg := strings.TrimSpace(strings.TrimLeft(cmd, "echo"))
 		fmt.Println(echo_msg)
+	case strings.HasPrefix(cmd, "type"):
+		var msg string
+		cmd_to_type := strings.TrimSpace(strings.TrimLeft(cmd, "type"))
+		if slices.Contains(builtin_cmds, cmd_to_type) {
+			msg = fmt.Sprintf("%s is a shell builtin\n", cmd_to_type)
+		} else {
+			msg = fmt.Sprintf("%s: not found\n", cmd_to_type)
+		}
+		fmt.Print(msg)
 	default:
 		msg := fmt.Sprintf("%s: command not found\n", cmd)
 		fmt.Print(msg)
